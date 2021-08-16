bfb447403cfffb6d22c3888f9537f9eb0fe9e156
SongYong1111 <994225036@qq.com>
2021-08-16 18:04:31 +0800

    第二天

---
+++ src/components/productCollection/ProductImage.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import { Image, Typography } from 'antd';
+
+interface PropsType {
+  id: string | number;
+  size: "large" | "small";
+  imageSrc: string;
+  price: string | number;
+  title: string;
+}
+
+export const ProductImage: React.FC<PropsType> = ({id, size, imageSrc, price, title}) => {
+  return (
+      <Fragment>
+        {size == "large" ? (
+          <Image src={imageSrc} height={285} width={490} />
+        ) : (
+          <Image src={imageSrc} height={120} width={240} />
+        )}
+        <div>
+          <Typography.Text type="secondary">
+            {title.slice(0,25)}
+          </Typography.Text>
+          <Typography.Text type="danger">
+            ￥ {price} 起
+          </Typography.Text>
+        </div>
+      </Fragment>
+  )
+}
